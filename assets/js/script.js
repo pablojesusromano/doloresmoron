fc61308f92dee12d4991346ef57bedc634589f4e
Pablo Romano <patoromano22@gmail.com>
2023-10-30 16:23:04 -0300

    responsive details

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,10 +1,15 @@
-/* anime({
-    targets: 'div',
-    translateX: 250,
-    rotate: '1turn',
-    backgroundColor: '#FFF',
-    duration: 800
-}); */
+// Selecciona todos los elementos de navegación
+let navItems = document.querySelectorAll('.nav-item');
 
-import { Fancybox } from "@fancyapps/ui";
-import "@fancyapps/ui/dist/fancybox/fancybox.css";
+// Itera sobre cada elemento
+navItems.forEach((item) => {
+    // Agrega un event listener para el evento 'click'
+    item.addEventListener('click', (event) => {
+        // Elimina la clase 'active' de todos los elementos
+        navItems.forEach((item) => {
+            item.classList.remove('active');
+        });
+        // Agrega la clase 'active' al elemento clickeado
+        event.currentTarget.classList.add('active');
+    });
+});
