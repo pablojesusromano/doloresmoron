824c00b87abd917cf204776260248746f765d416
Pablo Romano <patoromano22@gmail.com>
2023-11-02 20:33:22 -0300

    carga dinamica de cuadros

--- assets/js/script.js
+++ assets/js/script.js
@@ -13,3 +13,68 @@ navItems.forEach((item) => {
         event.currentTarget.classList.add('active');
     });
 });
+
+/* // Obtén una referencia al elemento div por su ID
+var miDiv = document.getElementById("miDiv");
+
+// Asigna una imagen de fondo al div
+miDiv.style.backgroundImage = "url('ruta/a/tu/imagen.jpg')"; */
+
+let carousel = document.querySelector('.carousel-inner');
+
+var carpeta = "./assets/img/";
+var images = [
+    { path: carpeta + "la-senadora.jpg", name: "LA SENADORA", shortName: "senadora", number: "" },
+    { path: carpeta + "mujer.jpg", name: "ADELA", shortName: "mujer", number: "" },
+    { path: carpeta + "que-bueno-sentirnos-hermanos.jpg", name: "QUE BUENO SENTIRNOS HERMANOS", shortName: "hermanos", number: "" },
+    { path: carpeta + "llegada-la-tarde-el-señor-dijo.jpg", name: "LLEGADA LA TARDE EL SEÑOR DIJO", shortName: "tarde", number: "" },
+    { path: carpeta + "comunicandonos-hoy.jpg", name: "COMUNICANDONOS HOY", shortName: "comunicandonos", number: "" },
+    { path: carpeta + "bigbang.jpg", name: "BIGBANG", shortName: "bigbang", number: "" },
+];
+
+let counter = 1;
+let pair = '';
+let flag = ' active';
+
+images.forEach(function (img) {
+    console.log(img);
+    if (counter > 2) {
+        counter = 1;
+    }
+    if (counter == 1) {
+        //console.log(img.path)
+        pair = pair + `
+        <div class="carousel-item${flag}">
+        <div class="row-nombre row justify-content-center mx-3 mt-5 align-items-center d-flex">
+        <div class="div-img div-imagen-${img.shortName} col-lg-6 col-md-12 col-sm-12 d-flex rounded-4 m-3" style="min-width: 320px;max-width: 600px;">
+            <div class="sombra-img position-absolute w-100 h-45 rounded-4"></div>
+            <a href="${img.path}" class="sin-estilo position-relative d-flex w-100" data-fancybox data-caption='"${img.name}"'>
+                <div class="texto-img position-absolute text-white m-2">
+                    <strong>"${img.name}"</strong>
+                </div>
+            </a>
+        </div>
+        `;
+        flag = '';
+    }
+    //console.log(pair);
+    if (counter == 2) {
+        pair = pair + `
+        <div class="div-img div-imagen-${img.shortName} col-lg-6 col-md-12 col-sm-12 d-flex rounded-4 m-3" style="min-width: 320px;max-width: 600px;">
+            <div class="sombra-img position-absolute w-100 h-45 rounded-4"></div>
+            <a href="${img.path}" class="sin-estilo position-relative d-flex w-100" data-fancybox data-caption='"${img.name}"'>
+                <div class="texto-img position-absolute text-white m-2">
+                    <strong>"${img.name}"</strong>
+                </div>
+            </a>
+        </div>
+        </div>
+        </div>
+        `;
+        carousel.innerHTML = pair;
+    }
+    //console.log(pair);
+
+    counter++;
+});
+
