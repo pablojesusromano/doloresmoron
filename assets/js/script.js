94964e4ad31a2ae372e11ff08379c6b58002944d
Pablo Romano <patoromano22@gmail.com>
2023-11-13 12:48:26 -0300

    position murales

--- assets/js/script.js
+++ assets/js/script.js
@@ -103,17 +103,17 @@ cuadros.forEach(function (img) {
 var carpetaMurales = "./assets/img/murales/";
 
 var murales = [
-    { path: carpetaMurales + "80-todas-las-manos-altar-catedral.jpg", name: '"TODAS LAS MANOS" (CATEDRAL COMODORO)', shortName: "todas-manos", number: "80" },
-    { path: carpetaMurales + "53-unp-rectorado.jpg", name: '"COMODORO" (RECTORADO UNIVERSIDAD NACIONAL DE LA PATAGONIA)', shortName: "rectorado", number: "53" },
-    { path: carpetaMurales + "75-la-vision-de-don-bosco-detalle.jpg", name: '"LA VISIÓN DE DON BOSCO" (CATEDRAL COMODORO)', shortName: "cristo", number: "75" },
-    { path: carpetaMurales + "79-la-anunciacion-ambon-catedral.jpg", name: '"LA ANUNCIACIÓN" (CATEDRAL COMODORO)', shortName: "anunciacion", number: "79" },
-    { path: carpetaMurales + "81-pescadores-de-hombres-altar-catedral.jpg", name: '"PESCADORES DE HOMBRES" (ALTAR CATEDRAL COMODORO)', shortName: "pescadores", number: "81" },
-    { path: carpetaMurales + "82-el-trigo-y-el-pan-de-vida.jpg", name: '"EL TRIGO Y EL PAN DE LA VIDA" (CATEDRAL COMODORO)', shortName: "trigo-pan", number: "82" },
-    { path: carpetaMurales + "83-las-vides-y-el-vino-altar-catedral.jpg", name: '"LAS VIDES Y EL VINO" (ALTAR CATEDRAL COMODORO)', shortName: "vino", number: "83" },
-    { path: carpetaMurales + "94-campo-y-trabajo-sociedad-rural.jpg", name: '"CAMPO Y TRABAJO" (SOCIEDAD RURAL)', shortName: "campo-trabajo", number: "94" },
-    { path: carpetaMurales + "95-vida-y-trabajo-municipalidad.jpg", name: '"VIDA Y TRABAJO" (MUNICIPALIDAD)', shortName: "vida-trabajo", number: "95" },
-    { path: carpetaMurales + "433-patagonia-tu-destino-es-levantar-vuelo-aeropuerto.jpg", name: '"PATAGONIA TU DESTINO ES LEVANTAR VUELO" (AEROPUERTO COMODORO)', shortName: "aeropuerto", number: "433" },
-    { path: carpetaMurales + "687-patagonia-tierra-generosa-2.jpg", name: '"PATAGONIA TIERRA GENEROSA" (SAN JULIÁN)', shortName: "patagonia-tierra", number: "687" },
+    { path: carpetaMurales + "80-todas-las-manos-altar-catedral.jpg", name: '"TODAS LAS MANOS" (CATEDRAL COMODORO)', shortName: "todas-manos", number: "80", position:"center" },
+    { path: carpetaMurales + "53-unp-rectorado.jpg", name: '"COMODORO" (RECTORADO UNIVERSIDAD NACIONAL DE LA PATAGONIA)', shortName: "rectorado", number: "53", position:"center" },
+    { path: carpetaMurales + "75-la-vision-de-don-bosco-detalle.jpg", name: '"LA VISIÓN DE DON BOSCO" (CATEDRAL COMODORO)', shortName: "cristo", number: "75", position:"top" },
+    { path: carpetaMurales + "79-la-anunciacion-ambon-catedral.jpg", name: '"LA ANUNCIACIÓN" (CATEDRAL COMODORO)', shortName: "anunciacion", number: "79", position:"top" },
+    { path: carpetaMurales + "81-pescadores-de-hombres-altar-catedral.jpg", name: '"PESCADORES DE HOMBRES" (ALTAR CATEDRAL COMODORO)', shortName: "pescadores", number: "81", position:"center" },
+    { path: carpetaMurales + "82-el-trigo-y-el-pan-de-vida.jpg", name: '"EL TRIGO Y EL PAN DE LA VIDA" (CATEDRAL COMODORO)', shortName: "trigo-pan", number: "82", position:"center" },
+    { path: carpetaMurales + "83-las-vides-y-el-vino-altar-catedral.jpg", name: '"LAS VIDES Y EL VINO" (ALTAR CATEDRAL COMODORO)', shortName: "vino", number: "83", position:"center" },
+    { path: carpetaMurales + "94-campo-y-trabajo-sociedad-rural.jpg", name: '"CAMPO Y TRABAJO" (SOCIEDAD RURAL)', shortName: "campo-trabajo", number: "94", position:"top" },
+    { path: carpetaMurales + "95-vida-y-trabajo-municipalidad.jpg", name: '"VIDA Y TRABAJO" (MUNICIPALIDAD)', shortName: "vida-trabajo", number: "95", position:"center" },
+    { path: carpetaMurales + "433-patagonia-tu-destino-es-levantar-vuelo-aeropuerto.jpg", name: '"PATAGONIA TU DESTINO ES LEVANTAR VUELO" (AEROPUERTO COMODORO)', shortName: "aeropuerto", number: "433", position:"center" },
+    { path: carpetaMurales + "687-patagonia-tierra-generosa-2.jpg", name: '"PATAGONIA TIERRA GENEROSA" (SAN JULIÁN)', shortName: "patagonia-tierra", number: "687", position:"center" },
 ];
 
 
@@ -126,7 +126,7 @@ murales.forEach(function (imgMural) {
     pairMurales = pairMurales + `
         <div style="height:auto;" class="carousel-item${flagMurales} w-100">
             <div class="row justify-content-center align-items-center d-flex">
-                <div class="div-img-mural div-imagen div-imagen-${imgMural.shortName} col-lg-6 col-md-12 col-sm-12 d-flex" style="min-width: 100%;max-width: 100%;">
+                <div class="div-img-mural div-imagen div-imagen-${imgMural.shortName} col-lg-6 col-md-12 col-sm-12 d-flex" style="min-width: 100%;max-width: 100%;background-position: center ${imgMural.position};">
                     <div class="sombra-img position-absolute w-100"></div>
                     <a style="height:auto;" href="${imgMural.path}" class="sin-estilo position-relative d-flex w-100" data-fancybox data-caption='${imgMural.name}'>
                         <div class="texto-img-mural text-white m-2">
